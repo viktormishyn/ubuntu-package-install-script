9b768ffc236306a4f10992e38f0f74be826f5c03
viktormishyn <viktormishyn@gmail.com>
2021-06-15 10:51:50 +0300

    Comment out PyCharm, WebStorm, Telegram

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 # to see favorite apps `dconf read /org/gnome/shell/favorite-apps`
 
-
 sudo apt update
 sudo apt upgrade
 
@@ -10,8 +9,7 @@ echo ""
 echo "==================================================Git"
 echo ""
 
-if git --version
-then
+if git --version; then
     echo "Git is already installed"
 else
     echo "Install Git..."
@@ -23,8 +21,7 @@ echo ""
 echo "==================================================Chrome"
 echo ""
 
-if google-chrome --version
-then 
+if google-chrome --version; then
     echo "Chrome browser is already installed"
 else
     echo "Install Chrome browser..."
@@ -34,52 +31,51 @@ else
     sudo rm -rf google-chrome-stable_current_amd64.deb
 fi
 
-if ! (dconf read /org/gnome/shell/favorite-apps | grep google-chrome)
-then gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'google-chrome.desktop']"
-fi
-    
-echo ""
-echo "==================================================PyCharm"
-echo ""
-
-echo "Install PyCharm IDE..."
-echo ""
-sudo snap install pycharm-community --classic
-
-if ! (dconf read /org/gnome/shell/favorite-apps | grep pycharm)
-then gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'pycharm-community_pycharm-community.desktop']"
+if ! (dconf read /org/gnome/shell/favorite-apps | grep google-chrome); then
+    gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'google-chrome.desktop']"
 fi
 
-echo ""
-echo "==================================================WebStorm"
-echo ""
+# echo ""
+# echo "==================================================PyCharm"
+# echo ""
 
-echo "Install WebStorm IDE"
-echo ""
-sudo snap install webstorm --classic
+# echo "Install PyCharm IDE..."
+# echo ""
+# sudo snap install pycharm-community --classic
 
-if ! (dconf read /org/gnome/shell/favorite-apps | grep webstorm)
-then gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'webstorm_webstorm.desktop']"
-fi
+# if ! (dconf read /org/gnome/shell/favorite-apps | grep pycharm)
+# then gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'pycharm-community_pycharm-community.desktop']"
+# fi
 
-echo ""
-echo "==================================================Telegram"
-echo ""
+# echo ""
+# echo "==================================================WebStorm"
+# echo ""
 
-echo "Install Telegram"
-echo ""
-sudo snap install telegram-desktop
+# echo "Install WebStorm IDE"
+# echo ""
+# sudo snap install webstorm --classic
 
-if ! (dconf read /org/gnome/shell/favorite-apps | grep telegram)
-then gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'telegram-desktop_telegram-desktop.desktop']"
-fi
+# if ! (dconf read /org/gnome/shell/favorite-apps | grep webstorm)
+# then gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'webstorm_webstorm.desktop']"
+# fi
+
+# echo ""
+# echo "==================================================Telegram"
+# echo ""
+
+# echo "Install Telegram"
+# echo ""
+# sudo snap install telegram-desktop
+
+# if ! (dconf read /org/gnome/shell/favorite-apps | grep telegram)
+# then gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'telegram-desktop_telegram-desktop.desktop']"
+# fi
 
 echo ""
 echo "==================================================Gnome-control-center"
 echo ""
 
-if gnome-control-center --version
-then 
+if gnome-control-center --version; then
     echo "Gnome-control-center is already installed"
 else
     echo "Install Gnome-control-center..."
@@ -99,13 +95,12 @@ echo ""
 
 # command name for shortcut: `flameshot gui`
 
-if flameshot --version
-then
+if flameshot --version; then
     echo "Flameshot is already installed"
 else
     echo "Install Flameshot..."
     echo ""
-	sudo apt install flameshot
+    sudo apt install flameshot
 fi
 
 echo ""
@@ -117,9 +112,3 @@ sudo snap install vlc
 echo ""
 echo "=================================================="
 echo ""
-
-
-
-
-
-
